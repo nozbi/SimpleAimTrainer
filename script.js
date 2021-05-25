538175fa8a2929917945f7c91a8029b47bfa187b
nozbi <76044507+nozbi@users.noreply.github.com>
2021-05-25 16:57:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 
+var Top;
+var Left;
+
+
 function RandomNumber(min, max){
     const r = Math.random()*(max-min) + min
     return Math.floor(r)
@@ -14,7 +18,16 @@ ChangeTargetPosition();
 }
 
 function OnResize(){
-
+var Target = document.getElementById("Target");
+var TargetHeight = Target.offsetHeight;
+var TargetWidth = Target.offsetWidth;
+var GamePanelHeight = document.getElementById("GamePanel").offsetHeight;
+var GamePanelWidth = document.getElementById("GamePanel").offsetWidth;
+var MaxTop = GamePanelHeight-TargetHeight;
+var MaxLeft = GamePanelWidth-TargetWidth;
+var Inaccuracy = 0.1;
+Target.style.left = Left + "vw";
+Target.style.top = Top + "vh";
 }
 
 function OnTargetClick(){
@@ -23,9 +36,24 @@ ChangeTargetPosition();
 
 function ChangeTargetPosition(){
 var Target = document.getElementById("Target");
-var Top = RandomNumber(0,100-4);
-var Left = RandomNumber(0,100-4);
+var TargetHeight = Target.offsetHeight;
+var TargetWidth = Target.offsetWidth;
+var GamePanelHeight = document.getElementById("GamePanel").offsetHeight;
+var GamePanelWidth = document.getElementById("GamePanel").offsetWidth;
+var MaxTop = GamePanelHeight-TargetHeight;
+var MaxLeft = GamePanelWidth-TargetWidth;
+var Inaccuracy = 0.1;
+Top = RandomNumber(0,PXToVH(MaxTop)-Inaccuracy);
+Left = RandomNumber(0,PXToVW(MaxLeft)-Inaccuracy);
 Target.style.left = Left + "vw";
 Target.style.top = Top + "vh";
+}
+
+function PXToVH(Px){
+return (100*Px)/document.getElementById("GamePanel").offsetHeight;
+}
+
+function PXToVW(Px){
+return (100*Px)/document.getElementById("GamePanel").offsetWidth;
 }
 
